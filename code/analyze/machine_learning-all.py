3313316c72314a2bfee3aa2ff5524cae2ffe9587
Juntao <juntao.jjt@alibaba-inc.com>
2019-10-09 13:59:12 +0800

    modified the machine_learning_code

--- code/analyze/machine_learning-all.py
+++ code/analyze/machine_learning-all.py
@@ -19,6 +19,11 @@ from sklearn.cluster import KMeans
 from sklearn.feature_selection import RFE
 from sklearn.externals import joblib
 import os
+from sklearn.model_selection import cross_val_score
+from sklearn.datasets import load_iris
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+
 
 def read_feature(para):
     new = np.load(para)
@@ -54,7 +59,9 @@ if __name__=="__main__":
         f.write(fs[i].split(".")[0]+"-"+str(scores)+"\n")
         f.close()
 
-
+    clf = AdaBoostClassifier(n_estimators=100)
+    scores = cross_val_score(clf, iris.data, iris.target, cv=5)
+    print(scores.mean())
 
 
 
